b5c76904ba0f65946e75f823ebdc26909168b540
shivxnshxrma <shivanshs773@gmail.com>
2025-10-25 22:17:44 +0530

    app.js changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "dotenv/config"
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB() 
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Error :",err);
+        throw err
+    })
+    app.listen(process.env.PORT),()=>{
+        console.log(`App is running on port ${process.env.PORT}`);
+    }
+})
+.catch((err)=>{
+    console.log("DB connection failed :",err);
+}) 
